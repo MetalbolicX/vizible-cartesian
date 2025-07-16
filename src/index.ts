4f8cd82b8e036d436ba697a65faa3fe2fff4f068
José Martínez Santana <jose.martinez.santana@gmail.com>
2025-07-15 22:28:36 -0600

    feat: add initial TypeScript configuration and export chart components
    - Created tsconfig.json with compiler options for TypeScript project.
    - Added exports for LineChart, lineChartOptions, and TimeChart in index.ts.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { LineChart } from "./charts/line/line-series.ts";
+import { type lineChartOptions } from "./charts/line/line-axes.ts";
+import { TimeChart } from "./charts/line/time-series.ts";
+
+export { LineChart, type lineChartOptions, TimeChart };
