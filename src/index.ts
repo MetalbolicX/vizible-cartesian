c3612711e593e690a64374e1329c9bc69a1ee6c7
José Martínez Santana <jose.martinez.santana@gmail.com>
2025-08-30 15:00:49 -0600

    refactor(types): add missing type exports for CartesianPlaneConfig and related chart configurations

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { LineChart } from "./charts/line/line-series.ts";
 import { TimeChart } from "./charts/line/time-series.ts";
 import { ScatterChart } from "./charts/scatter/scatter-plot.ts";
 import { CustomScatterChart } from "./charts/scatter/custom-scatter-plot.ts";
+
 import type {
   CustomerScatterChartOptions,
   LineChartOptions,
   ScatterChartOptions,
   SeriesOptions,
+  CartesianPlaneConfig,
+  ScatterChartConfig,
+  CustomScatterChartConfig,
 } from "./types.ts";
 
 export {
@@ -18,4 +22,7 @@ export {
   type LineChartOptions,
   type ScatterChartOptions,
   type SeriesOptions,
+  type CartesianPlaneConfig,
+  type ScatterChartConfig,
+  type CustomScatterChartConfig,
 };
